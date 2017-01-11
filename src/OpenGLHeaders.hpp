d2911b59388ae45319eaabe28b27fff9203c1085
Reewr <alexander.imenes@gmail.com>
2017-01-11 11:18:43 +0100

    Adds newline between includes in OpenGLHeaders
    This is so that clang-format does not reorder the includes. It is very
    important, crucial in fact, that the OGL header comes before the GLFW
    header. The whole reason for the OpenGLHeaders.hpp file is to make sure
    that this always happens.
    
    FYI, including GLFW before OGL will cause a compile time error where
    OGL complains about GL headers included prior to it being included.

--- src/OpenGLHeaders.hpp
+++ src/OpenGLHeaders.hpp
@@ -2,6 +2,9 @@
 #define CUSTOM_OPENGL_HEADERS
 
 #include <OGL/OGL-3-3.hpp>
+
+// OGL must always be first. Add line break so that clang-format doesnt
+// reorder the order of them.
 #include <GLFW/glfw3.h>
 
 #endif
