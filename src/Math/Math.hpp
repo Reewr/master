2bfa79e4cbfa9e98fb26a188c830f21bbebb7e52
Thhethssmuz <thhethssmuz@gmail.com>
2017-01-15 21:32:14 +0100

    replace old math with mmm

--- src/Math/Math.hpp
+++ src/Math/Math.hpp
@@ -1,29 +1,4 @@
-#ifndef MATH_HPP
-#define MATH_HPP
+#pragma once
 
-#include <cmath>
-#include <iostream>
-
-#include "basic/basic.hpp"
-
-#include "swizzle/swizzle.hpp"
-#include "vec2/vec2.hpp"
-#include "vec3/vec3.hpp"
-#include "vec4/vec4.hpp"
-
-#include "mat3/mat3.hpp"
-#include "mat4/mat4.hpp"
-
-#include "swizzle/swizzle.tpp"
-#include "vec2/vec2.tpp"
-#include "vec3/vec3.tpp"
-#include "vec4/vec4.tpp"
-
-#include "mat3/mat3.tpp"
-#include "mat4/mat4.tpp"
-
-#include "extra/MVP.hpp"
-#include "extra/camera.hpp"
-#include "extra/print.hpp"
-
-#endif
+#include <mmm.hpp>
+using namespace mmm;
