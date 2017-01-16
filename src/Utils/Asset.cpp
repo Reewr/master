9cc62d3e0d65897c706bff3bb0b50e015e6c9f99
Reewr <alexander.imenes@gmail.com>
2017-01-16 21:30:22 +0100

    Asset is kind of global state

--- src/Utils/Asset.cpp
+++ src/Utils/Asset.cpp
@@ -1,1 +1,54 @@
 #include "Asset.hpp"
+#include "Utils.hpp"
+
+Asset::Asset(CFG* c)
+    : mCFG(c)
+    , mInput(nullptr)
+    , mEngine(nullptr)
+    , mState(nullptr) {}
+
+Asset::Asset(CFG* c, Input::Input* i, Engine* e, State* s)
+    : mCFG(c)
+    , mInput(i)
+    , mEngine(e)
+    , mState(s) {}
+
+CFG* Asset::cfg() {
+  if (mCFG == nullptr)
+    throw Error("Tried to access CFG when nullptr");
+  return mCFG;
+}
+
+Input::Input* Asset::input() {
+  if (mInput == nullptr)
+    throw Error("Tried to access Input when nullptr");
+  return mInput;
+}
+
+Engine* Asset::engine() {
+  if (mEngine == nullptr)
+    throw Error("Tried to access Engine when nullptr");
+  return mEngine;
+}
+
+State* Asset::state() {
+  if (mState == nullptr)
+    throw Error("Tried to access State when nullptr");
+  return mState;
+}
+
+void Asset::setCFG(CFG* c) {
+  mCFG = c;
+}
+
+void Asset::setInput(Input::Input* i) {
+  mInput = i;
+}
+
+void Asset::setEngine(Engine* e) {
+  mEngine = e;
+}
+
+void Asset::setState(State* s) {
+  mState = s;
+}
--- src/Utils/Asset.hpp
+++ src/Utils/Asset.hpp
@@ -1,10 +1,30 @@
 #pragma once
 
 #include "CFG.hpp"
+#include "../Engine.hpp"
+#include "../State/State.hpp"
+#include "../Input/Input.hpp"
 
 //! Asset is a class that is being sent around that stores a lot of useful
 //! settings.
 class Asset {
 public:
-  CFG cfg;
+  Asset(CFG* c);
+  Asset(CFG* c, Input::Input* i, Engine* e, State* s);
+
+  CFG* cfg();
+  Input::Input* input();
+  Engine* engine();
+  State* state();
+
+  void setCFG(CFG* c);
+  void setInput(Input::Input* i);
+  void setEngine(Engine* e);
+  void setState(State* s);
+
+private:
+  CFG* mCFG;
+  Input::Input* mInput;
+  Engine* mEngine;
+  State* mState;
 };
