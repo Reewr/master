eae1b0ec9be284156a242a8f36df75838a333f93
Reewr <alexander.imenes@gmail.com>
2017-01-17 16:40:12 +0100

    Adds working console

--- src/Console/Console.cpp
+++ src/Console/Console.cpp
@@ -15,12 +15,20 @@
 #include "../Utils/Asset.hpp"
 
 Console::Console(Asset* asset) {
-  vec2 res = asset->cfg()->graphics.res;
-  mAsset = asset;
-  mCurrentText = "";
-  mBoundingBox = Rect(0, 0, res.x, res.y / 2);
-  mRect        = new GL::Rectangle(mBoundingBox);
-  mText        = new Text(mFont, "> _", vec2(10, res.y / 2), 20, Text::WHITE);
+  vec2 res      = asset->cfg()->graphics.res;
+  vec2 textPos  = vec2(10, res.y / 2 - 30);
+  vec2 errorPos = vec2(10, res.y / 2);
+
+  mAsset           = asset;
+  mCurrentText     = "";
+  mBoundingBox     = Rect(0, 0, res.x, res.y / 2);
+  mRect            = new GL::Rectangle(mBoundingBox);
+  mAutoCompleteBox = new GL::Rectangle(Rect(0, 0, 0, 0));
+  mText            = new Text(mFont, "> _", textPos, 20, Text::WHITE);
+  mError           = new Text(mFont, "Error:", errorPos, 20, Text::RED);
+  mErrorDisplayed  = 0;
+
+  mError->isVisible(false);
 
   // Specific program for the console since the console
   // is just drawn in black with alpha
@@ -28,7 +36,7 @@ Console::Console(Asset* asset) {
   mProgram->bindAttribs({ "position", "texcoord" }, { 0, 1 });
   mProgram->link();
   mProgram->setUniform("screenRes", res, "guiOffset", vec2());
-  mProgram->setUniform("guiColor", vec4(0, 0, 0, 0.7));
+  mProgram->setUniform("guiColor", vec4(0, 0, 0, 0.9));
 }
 
 Console::~Console() {
@@ -36,23 +44,198 @@ Console::~Console() {
   delete mRect;
   delete mText;
   delete mError;
+  delete mAutoCompleteBox;
 
-  for (auto a : mHistory)
+  for (auto a : mAutoComplete)
     delete a;
+
+  for (auto h : mHistory)
+    delete h;
+
+  mAutoComplete.clear();
+  mHistory.clear();
+  mCommands.clear();
+}
+
+/**
+ * @brief
+ *   This function adds a new command that can be invoked from
+ *   the console using the given name.
+ *
+ *   All names should have a namespace followed by a `.`. Example:
+ *   config.res
+ *
+ *   The return value of the Console::Handler function is a string,
+ *   which when not empty, will print an error onto the screen.
+ *
+ * @param name
+ *   Name of the function, is unique.
+ *
+ * @param h
+ *   The handler itself.
+ */
+void Console::addCommand(const std::string& name, Console::Handler h) {
+  if (name.find(".") == std::string::npos)
+    throw new Error("Must have namespace in name. Ex: config.res");
+
+  mCommands[name] = h;
+  mAutoComplete.push_back(new Text(mFont, name, vec2(0, 0), 20, Text::WHITE));
+}
+
+/**
+ * @brief
+ *   Sets an error just below where the user is typing in a
+ *   red text that is displayed for three seconds before
+ *   being hidden again. Will always have the words "Error: " infront
+ *   of it.
+ *
+ * @param message
+ *   The message to display
+ */
+void Console::setError(const std::string& message) {
+  mError->setText("Error: " + message);
+  mError->isVisible(true);
+  mErrorDisplayed = 3;
+}
+
+/**
+ * @brief
+ *   When a command has been executed, this function
+ *   is called which adds the current text to a list of
+ *   history as a new Text element.
+ *
+ *   This is displayed on the screen until it goes outside it.
+ *   A text element will gradiually be moved upwards until
+ *   it is outside of the screen, at which point it will be hidden
+ */
+void Console::addHistory() {
+  vec2  pos = vec2(10, mAsset->cfg()->graphics.res.y / 2 - 60);
+  Text* t   = new Text(mFont, mCurrentText, pos, 20, Text::WHITE);
+
+  t->isVisible(true);
+
+  int index = mHistory.size();
+  for (auto a : mHistory) {
+    vec2 newPos = vec2(0, -index * 30);
+
+    if (newPos.y + pos.y < 0)
+      a->isVisible(false);
+
+    a->setOffset(newPos);
+    index--;
+  }
+
+  log("Adding ", mCurrentText, " to history");
+
+  mHistory.push_back(t);
+}
+
+/**
+ * @brief
+ *   Checks the current text for errors, if any, prints error.
+ *   If there are no errors and the text matches a command
+ *   it will call that command with the text within the brackets.
+ *
+ *   Good commands:
+ *
+ *     config.res(100, 100))
+ *     config.res.x(100)
+ *     config.res.y(100)
+ *
+ *   Bad commands:
+ *     config.res = 100
+ *     config.res(10
+ *     config.res)(
+ *
+ * @param event
+ */
+void Console::doCommand(const Input::Event& event) {
+  size_t startBracket = mCurrentText.find_first_of("(");
+  size_t endBracket   = mCurrentText.find_last_of(")");
+
+  if (startBracket == std::string::npos)
+    return setError("Commands must use brackets");
+
+  if (startBracket > endBracket)
+    return setError("Bracket alignment incorrect");
+
+  if (endBracket != mCurrentText.size() - 1)
+    return setError("Commands must end with a closing bracket");
+
+  std::string command = mCurrentText.substr(0, startBracket);
+  std::string parameters = mCurrentText.substr(startBracket + 1, endBracket);
+  parameters.pop_back();
+
+  if (mCommands.count(command) == 0)
+    return setError("No such command '" + command + "'");
+
+  std::string output = mCommands[command](mAsset, event, parameters);
+
+  if (output != "")
+    return setError(output);
+
+  addHistory();
+  setText("");
+}
+
+/**
+ * @brief
+ *   Sets the text of the console, also starts
+ *   the autocomplete.
+ *
+ * @param s
+ */
+void Console::setText(const std::string& s) {
+  mCurrentText = s;
+  mText->setText("> " + mCurrentText + "_");
+  setAutoComplete();
+}
+
+/**
+ * @brief
+ *   Checks the current text to see if there are any commands
+ *   that contain the words in them. If so, display those
+ *   commands.
+ *
+ *   Also adjusts the autocompletion box to match width
+ *   and height of the texts.
+ */
+void Console::setAutoComplete() {
+  if (mCommands.size() == 0 || mCurrentText.size() == 0) {
+    mShowAutoComplete = false;
+    return;
+  }
+
+  vec2 res      = mAsset->cfg()->graphics.res;
+  Rect box      = Rect(5, res.y / 2 + 30, 0, 0);
+  int  index    = 0;
+  vec2 startPos = vec2(10, res.y / 2 + 30);
+
+  for (auto a : mAutoComplete) {
+    bool hasText = a->getText().find(mCurrentText) != std::string::npos;
+    if (hasText) {
+      a->isVisible(true);
+      a->setOffset(startPos + vec2(0, 20 * index));
+      box.size.x = max(a->size().x, box.size.x);
+      box.size.y = max(a->size().y, box.size.y);
+      index++;
+    } else {
+      a->isVisible(false);
+    }
+  }
+
+  if (box.size.y > 0 && index != 0) {
+    box.size.x = box.size.x + 10;
+    mShowAutoComplete = true;
+    mAutoCompleteBox->change(box);
+  } else {
+    mShowAutoComplete = false;
+  }
 }
 
 /**
  * @brief
  *   Handles the key input for a console
- *
- * @param key
- *   The glfw key code of the key that was pressed
- *
- * @param action
- *   The action taken
- *
- * @param mods
- *   A bit flagged int with the mods that are clicked
  *
  * @return
  */
@@ -73,31 +256,64 @@ void Console::input(const Input::Event& event) {
     return event.stopPropgation();
   }
 
-  if (event.key() == GLFW_KEY_BACKSPACE) {
+  if (event.keyPressed(GLFW_KEY_BACKSPACE)) {
     if (mCurrentText.length() > 0)
       mCurrentText.pop_back();
 
-    mText->setText("> " + mCurrentText + "_");
+    setText(mCurrentText);
     return event.stopPropgation();
   }
 
   if (event.keyPressed(GLFW_KEY_ENTER)) {
-    log("Info should perform action with '" + mCurrentText + "'");
+    doCommand(event);
     return event.stopPropgation();
   }
 
   if (event == Input::Event::Type::CharacterInput) {
-    mCurrentText.append(event.character());
-    mText->setText("> " + mCurrentText + "_");
+    setText(mCurrentText + event.character());
     return event.stopPropgation();
   }
 }
 
-void Console::draw(float) {
+/**
+ * @brief
+ *   Updates the console. This only does one thing
+ *   at the moment which is count down the time
+ *   it should display the error, if it is displayed
+ *
+ * @param deltaTime
+ */
+void Console::update(float deltaTime) {
+  if (mErrorDisplayed > 0)
+    mErrorDisplayed = mErrorDisplayed - deltaTime;
+
+
+  if (mErrorDisplayed <= 0 && mError->isVisible()) {
+    mError->isVisible(false);
+    mErrorDisplayed = 0;
+  }
+}
+
+/**
+ * @brief Draw!
+ */
+void Console::draw() {
   if (!isVisible())
     return;
 
   mProgram->bind();
   mRect->draw();
+
+  if (mShowAutoComplete) {
+    mAutoCompleteBox->draw();
+    for(auto a : mAutoComplete)
+      a->draw();
+  }
+
   mText->draw();
+  mError->draw();
+
+  for (auto a : mHistory) {
+    a->draw();
+  }
 }
--- src/Console/Console.hpp
+++ src/Console/Console.hpp
@@ -23,7 +23,6 @@ class Rectangle;
 
 class Console : public GUI {
 public:
-
   using Command = std::string;
 
   // Typedefs a handler as a function that takes in an asset,
@@ -33,24 +32,58 @@ public:
   //
   // The return type is a string. If the string is not empty, it is
   // considered to be an error string that will be shown to the user
-  using Handler = std::function<std::string(Asset* asset, const std::string& input)>;
+  using Handler = std::function<std::string(Asset* asset,
+                                            const Input::Event& event,
+                                            const std::string& input)>;
 
   //! Create console
   Console(Asset* asset);
   ~Console();
 
+  //! Adds a command to the console that can be invoked
+  //! using the name followed by ().
+  //! Ex: config.res(1024, 768)
+  void addCommand(const std::string& name,
+                  Handler h);
+
+  //! Sets an error on the console
+  void setError(const std::string& message);
+
+  // Input handler
   void input(const Input::Event& event);
+
+  // Sets the text of the console
+  void setText(const std::string& s);
+
+  // Update handler
+  void update(float dt);
+
   //! Draw the console
-  void draw(float dt);
+  void draw();
 
 private:
+  // Checks if the command is legal, prints error if not.
+  void doCommand(const Input::Event& event);
+
+  // Sets the autocomplete by checking the current string
+  void setAutoComplete();
+
+  // When a command has been executed, add it to history
+  void addHistory();
+
+  float mErrorDisplayed;
+
   std::string    mCurrentText;
   Text*          mText;
   Text*          mError;
   Program*       mProgram;
   GL::Rectangle* mRect;
+  GL::Rectangle* mAutoCompleteBox;
   Asset*         mAsset;
 
+  bool mShowAutoComplete;
+  std::vector<Text*> mAutoComplete;
   std::vector<Text*> mHistory;
-  std::map<Command, Handler> mCommands;
+
+  std::map<std::string, Handler> mCommands;
 };
