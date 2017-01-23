85709d7acfb5cd07e53ff4a2fa69bc1bbc1377b7
Reewr <alexander.imenes@gmail.com>
2017-01-23 22:18:03 +0100

    Makes Rectangle constexpr

--- src/Shape/Rectangle.cpp
+++ src/Shape/Rectangle.cpp
@@ -1,51 +1,13 @@
 #include "Rectangle.hpp"
 
-#include <cmath>
-
-Rectangle::Rectangle() {}
-
-Rectangle::Rectangle(vec2 tl, vec2 s) : topleft(tl), size(s) {}
-
-Rectangle::Rectangle(float x, float y, float sx, float sy) {
-  topleft = vec2(x, y);
-  size    = vec2(sx, sy);
-}
-
-Rectangle::Rectangle(vec2 tl, float sx, float sy) {
-  topleft = tl;
-  size    = vec2(sx, sy);
-}
-
-Rectangle::Rectangle(const Rectangle& r) {
-  topleft = r.topleft;
-  size    = r.size;
-}
-
-bool Rectangle::contains(const vec2& p) const {
-  return p.x >= topleft.x && p.y >= topleft.y && p.x <= topleft.x + size.x &&
-         p.y <= topleft.y + size.y;
-}
-
-vec2 Rectangle::middle() const {
-  return vec2(topleft.x + size.x / 2, topleft.y + size.y / 2);
-}
-
-void Rectangle::middle(const vec2& m) {
-  bottomright(m * 2);
-}
-
-vec2 Rectangle::bottomright() const {
-  return vec2(topleft.x + size.x, topleft.y + size.y);
-}
-
 void Rectangle::bottomright(const vec2& br) {
   vec2 s = br - topleft;
   if (s.x < 0) {
-    s.x       = fmax(topleft.x, br.x) - fmin(topleft.x, br.x);
+    s.x       = max(topleft.x, br.x) - min(topleft.x, br.x);
     topleft.x = br.x;
   }
   if (s.y < 0) {
-    s.y       = fmax(topleft.y, br.y) - fmin(topleft.y, br.y);
+    s.y       = max(topleft.y, br.y) - min(topleft.y, br.y);
     topleft.y = br.y;
   }
   size = s;
--- src/Shape/Rectangle.hpp
+++ src/Shape/Rectangle.hpp
@@ -6,15 +6,47 @@ struct Rectangle {
   vec2 topleft;
   vec2 size;
 
-  Rectangle();
-  Rectangle(vec2 tl, vec2 s);
-  Rectangle(vec2 tl, float sx, float sy);
-  Rectangle(float x, float y, float sx, float sy);
-  Rectangle(const Rectangle& r);
+  constexpr Rectangle();
+  constexpr Rectangle(const vec2& tl, const vec2& s);
+  constexpr Rectangle(const vec2& tl, float sx, float sy);
+  constexpr Rectangle(float x, float y, float sx, float sy);
+  constexpr Rectangle(const Rectangle& r);
 
-  bool contains(const vec2& point) const;
-  vec2 middle() const;
-  void middle(const vec2& m);
-  vec2 bottomright() const;
+  constexpr bool contains(const vec2& point) const;
+  constexpr vec2 middle() const;
+  constexpr vec2 bottomright() const;
   void bottomright(const vec2& br);
 };
+
+// ----------------------------------------------------------
+//
+// Below follows the implementation of the constexpr functions
+//
+// ----------------------------------------------------------
+
+constexpr Rectangle::Rectangle() {}
+
+constexpr Rectangle::Rectangle(const vec2& tl, const vec2& s)
+    : topleft(tl), size(s) {}
+
+constexpr Rectangle::Rectangle(float x, float y, float sx, float sy)
+    : topleft(x, y), size(sx, sy) {}
+
+constexpr Rectangle::Rectangle(const vec2& tl, float sx, float sy)
+    : topleft(tl), size(sx, sy) {}
+
+constexpr Rectangle::Rectangle(const Rectangle& r)
+    : topleft(r.topleft), size(r.size) {}
+
+constexpr bool Rectangle::contains(const vec2& p) const {
+  return p.x >= topleft.x && p.y >= topleft.y && p.x <= topleft.x + size.x &&
+         p.y <= topleft.y + size.y;
+}
+
+constexpr vec2 Rectangle::middle() const {
+  return vec2(topleft.x + size.x / 2, topleft.y + size.y / 2);
+}
+
+constexpr vec2 Rectangle::bottomright() const {
+  return vec2(topleft.x + size.x, topleft.y + size.y);
+}
