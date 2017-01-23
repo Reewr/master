7f646eee481e264aee9d17c055984d00abbac184
Reewr <alexander.imenes@gmail.com>
2017-01-23 15:38:29 +0100

    Removes tpp file since it was so tiny.

--- src/GLSL/Program.hpp
+++ src/GLSL/Program.hpp
@@ -115,4 +115,37 @@ private:
   bool isUsable = false;
 };
 
-#include <GLSL/Program.tpp>
+// ----------------------------------------------------------
+//
+// Below follows the implementation of the template functions
+//
+// ----------------------------------------------------------
+template <typename T>
+bool Program::setUniform(const std::string& uni, const T& t) {
+  bind();
+  GLint loc = getUniformLocation(uni);
+  if (loc != -1)
+    return setGLUniform(loc, t);
+  return false;
+}
+
+template <typename T>
+bool Program::setUniforms(const std::vector<std::string>& unis,
+                          const std::vector<T>&           t) {
+  if (unis.size() != t.size()) {
+    error("setUniforms: size is not equal");
+    return false;
+  }
+
+  for (unsigned int i = 0; i < unis.size(); i++) {
+    if (!setUniform(unis[i], t[i]))
+      return false;
+  }
+  return true;
+}
+
+template <typename T, typename... Ts>
+bool Program::setUniform(const std::string& uni, T x, Ts... xs) {
+  setUniform(uni, x);
+  return setUniform(xs...);
+}
