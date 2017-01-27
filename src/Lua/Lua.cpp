9cc6990adc1f46a2f499f2b1113c1f373078b240
Reewr <alexander.imenes@gmail.com>
2017-01-27 14:28:11 +0100

    Adds documentation to Lua

--- src/Lua/Lua.cpp
+++ src/Lua/Lua.cpp
@@ -16,12 +16,32 @@
 
 #include <sol.hpp>
 
+/**
+ * @brief
+ *   Compare two Typenames structures, returning
+ *   a result so that they are sorted alphabetically.
+ *
+ * @param t1
+ * @param t2
+ *
+ * @return
+ */
 bool compare_by_word(const Lua::Typenames& t1, const Lua::Typenames t2) {
   return t1.name < t2.name;
 }
 
 namespace Lua {
 
+  /**
+   * @brief
+   *   Returns the name of a type, "Undef" if a type cannot
+   *   be matched. The type names are common names for variables,
+   *   such as 'Num' for 'Number'
+   *
+   * @param t
+   *
+   * @return
+   */
   std::string solTypetoStr(sol::type t) {
     switch(t) {
       case sol::type::none:
@@ -59,14 +79,35 @@ namespace Lua {
     }
   }
 
+  /**
+   * @brief
+   *   Equal operator for Typenames
+   *
+   * @param rhs
+   *
+   * @return
+   */
   bool Typenames::operator==(const Typenames& rhs) const {
     return name == rhs.name && typeName == rhs.typeName;
   }
 
+  /**
+   * @brief
+   *   Initializes the Lua class, creating the Lua State and exporting
+   *   several C++ objects to Lua.
+   */
   Lua::Lua() : mConsole(nullptr), mCFG(nullptr) { reInitialize(); }
 
   Lua::~Lua() {}
 
+  /**
+   * @brief
+   *   This recreates the state.
+   *   It will also try to load the `main.lua` file located in `./lua`.
+   *
+   *   This function is also used by the constructor. If used after this,
+   *   it will reload everything.
+   */
   void Lua::reInitialize() {
     engine = sol::state();
     engine.open_libraries(sol::lib::base, sol::lib::package);
@@ -98,9 +139,61 @@ namespace Lua {
     loadFile("lua/main.lua");
   }
 
+  /**
+   * @brief
+   *   Tells the Lua engine to add the console to global scope
+   *   as a `console` variable, making it available to Lua code.
+   *
+   *   It will store the pointer to the console so that when
+   *   `reinitialize` is done, it can include it again.
+   *
+   * @param console
+   */
+  void Lua::add(Console* console) {
+    if (console == nullptr)
+      return;
+
+    engine["console"] = console;
+    mConsole          = console;
+  }
+
+  /**
+   * @brief
+   *   Tells the Lua engine to add the cfg to global scope
+   *   as a `cfg` variable, making it available to Lua code.
+   *
+   *   It will store the pointer to the cfg so that when
+   *   `reinitialize` is done, it can include it again.
+   *
+   * @param cfg
+   */
+  void Lua::add(CFG* cfg) {
+    if (cfg == nullptr)
+      return;
+
+    engine["cfg"] = cfg;
+    mCFG          = cfg;
+  }
+
+
+  /**
+   * @brief
+   *   This will try to load a filename. If it cannot find the file,
+   *   or if the file fails to load due to syntax errors,
+   *   it will output an error.
+   *
+   * @param filename
+   *
+   * @return
+   */
   bool Lua::loadFile(const std::string& filename) {
+    // Any errors that are most likely due to not being able to
+    // find the file.
+    //
+    // Output errors to console, if it exists, instead of
+    // our error function
     try {
-      sol::function_result res     = engine.script_file("lua/main.lua");
+      sol::function_result res     = engine.script_file(filename);
       bool                 isValid = res.valid();
 
       if (!isValid && mConsole != nullptr)
@@ -120,6 +213,16 @@ namespace Lua {
     }
   }
 
+  /**
+   * @brief
+   *   Checks if a type should be included based on several
+   *   checks.
+   *
+   * @param name the name of the variable
+   * @param search the search that the user is doing
+   *
+   * @return
+   */
   bool Lua::shouldIncludeType(const std::string& name,
                               const std::string& search) {
     if (name.find("sol.") == 0)
@@ -137,6 +240,15 @@ namespace Lua {
     return name.find(search) != std::string::npos;
   }
 
+  /**
+   * @brief
+   *   Turns a pair, retrieved from iterating over tables/states,
+   *   into a Typenames structure.
+   *
+   * @param pair
+   *
+   * @return
+   */
   Typenames
   Lua::getTypename(std::pair<sol::basic_object<sol::reference>,
                              sol::basic_object<sol::reference>>& pair) {
@@ -150,6 +262,19 @@ namespace Lua {
     return {solTypetoStr(firstType), solTypetoStr(secondType)};
   }
 
+  /**
+   * @brief
+   *   If this is given a table and a search string that contains `:`, it will
+   *   recrusively try to find the variables that is valid for the scope.
+   *
+   *   For example, given a variable `MyTable:MySubTable:MySubSubTable`, it
+   *   will return the variables that are available to `MySubSubTable`
+   *
+   * @param t
+   * @param name
+   *
+   * @return
+   */
   std::vector<Typenames> Lua::getScope(sol::table& t, const std::string& name) {
     size_t pos = name.find_first_of(":");
     std::vector<Typenames> types = {};
@@ -176,10 +301,22 @@ namespace Lua {
     return types;
   }
 
+  /**
+   * @brief
+   *   Returns the variables that matches the name. If name is an empty
+   *   string it will return all variables that are available in
+   *   the global scope.
+   *
+   * @param name
+   *
+   * @return
+   */
   std::vector<Typenames> Lua::getTypenames(const std::string name) {
     size_t pos = name.find_first_of(":");
     std::vector<Typenames> types = {};
 
+    // No scoping, just retrieve the variables
+    // that match name in global scope
     if (pos == std::string::npos) {
       for(auto a : engine) {
         Typenames t = getTypename(a);
@@ -192,12 +329,17 @@ namespace Lua {
       return types;
     }
 
+    // There's scoping to be done. Find the variables that are
+    // valid under that scope.
     std::string scopeName = name.substr(0, pos);
     std::string rest = name.substr(pos + 1);
 
     sol::object obj = engine[scopeName];
 
     switch(obj.get_type()) {
+
+      // Userdata has a special handler since in order to retrieve the variables
+      // you have to check the metatable instead of iterating over keys
       case sol::type::userdata: {
         sol::table userData = obj;
         sol::table meta     = userData[sol::metatable_key];
@@ -211,6 +353,9 @@ namespace Lua {
         }
         break;
       }
+
+      // Tables are easy to retrieve, since you can just keep
+      // iterating over key/values
       case sol::type::table: {
         sol::table table = obj;
 
@@ -230,19 +375,4 @@ namespace Lua {
     return types;
   }
 
-  void Lua::add(Console* console) {
-    if (console == nullptr)
-      return;
-
-    engine["console"] = console;
-    mConsole          = console;
-  }
-
-  void Lua::add(CFG* cfg) {
-    if (cfg == nullptr)
-      return;
-
-    engine["cfg"] = cfg;
-    mCFG          = cfg;
-  }
 }
--- src/Lua/Lua.hpp
+++ src/Lua/Lua.hpp
@@ -12,17 +12,25 @@ namespace Input {
 
 namespace Lua {
 
+  // Turns a sol::type to a string of max 6 characters
   std::string solTypetoStr(sol::type t);
 
+  // This is a simple struct used to make it easier to
+  // store the variables available in lua
   struct Typenames {
     std::string name;
     std::string typeName;
 
+    // equal operator
     bool operator==(const Typenames& rhs) const;
   };
 
   class Lua {
   public:
+
+    // Creates a Lua instance that holds the Lua State.
+    // This allows you to load files that will have C++ object representations
+    // in them
     Lua();
     ~Lua();
 
@@ -37,16 +45,30 @@ namespace Lua {
     //! Recreates the lua engine, adding the console and config
     //! objects and reruns the files that are loaded
     void reInitialize();
+
+    // loads a file by filename/path. This is according to where
+    // the working directory is.
     bool loadFile(const std::string& filename);
+
+    // Returns a list of types that is accessible based on the name given.
+    // For instance, a name with "" will check global scope where as
+    // a name with "config:" will check the scope of "config"
     std::vector<Typenames> getTypenames(const std::string name="");
 
     sol::state engine;
 
   private:
-    bool shouldIncludeType(const std::string& name, const std::string& search);
+
+    // Simple way of creating a Typenames struct based on a pair given
+    // by a table.
     Typenames getTypename(std::pair<sol::basic_object<sol::reference>,
                                     sol::basic_object<sol::reference>>& a);
+
+    // Checks the scope, this function will work recurisvely.
     std::vector<Typenames> getScope(sol::table& t, const std::string& name);
+    // Function used by both getTypenames and getScope to see if they should
+    // ignore a variable based on what the name contains / does not contain
+    bool shouldIncludeType(const std::string& name, const std::string& search);
     Console* mConsole;
     CFG*     mCFG;
   };
