4c87ffbf1599116fc7d1f17387ccb1ea8da53af2
Reewr <alexander.imenes@gmail.com>
2017-01-31 14:24:45 +0100

    Fixes OpenGL error

--- src/Shape/GL/Cube.cpp
+++ src/Shape/GL/Cube.cpp
@@ -66,7 +66,8 @@ void GLCube::setup() {
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glEnableAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), 0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), 0);
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER,
