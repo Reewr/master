c3a22c7017370e1c6eef08ae28c2dc89c948bd08
Reewr <alexander.imenes@gmail.com>
2017-02-02 23:16:12 +0100

    Adds more documentation comments

--- lua/console/main.lua
+++ lua/console/main.lua
@@ -1,5 +1,30 @@
--- This is the main file of the console. Usually
--- this is the only file you need to require.
+--[[
+  This file contains everything that depends on
+  the console in order to work.
+
+  A lot of the functions within this file, and the files
+  that this requires, is utility and debug functions that
+  doesnt add anything to do the game, but helps with developing.
+
+  The module exposes a couple of global variables:
+
+  - `state`
+  - `hud`
+
+  The `state` object contains functions that helps with
+  interacting with states through events.
+
+  The `hud` object contains various hud element objects.
+  For instance, it contains `fps`, which has `hide` and `show`
+  functions to hide/show the FPS on the screen.
+
+  Lastly, it overrides the normal `print` function with one
+  that prints to both the Engine's stdout and to the console.
+
+  This module is only loaded if `cfg.console.enabled` is true
+
+  @class module
+]]
 
 -- These are the global values that the console
 -- exposes
@@ -21,19 +46,45 @@ function print(...)
   console:log('\\<102,217,239,255:>LUA:\\</> ' .. s)
 end
 
--- Exists the entire game.
+--[[
+
+  Exists the entire game by sending a state change
+  to the Engine. Will stop everything and close the engine
+
+  This function is only available if `cfg.console.enabled` is true.
+
+  @class function
+]]
 function exit()
   console:log("Quitting")
   currentEvent:sendStateChange(States.QuitAll)
   currentEvent:stopPropgation()
 end
 
--- Synonomous with quit
+--[[
+
+  As with `exit()`, thus function just exits the game.
+  It is also only available if `cfg.console.enabled` is true.
+
+  @see exit
+  @class function
+]]
 function quit()
   exit()
 end
 
--- Reloads the Lua Engine
+--[[
+
+  Reloads the Lua Engine by sending a state change
+  to the Engine.
+
+  If variables are defined by using the console and not
+  stored in a file, they will be erased.
+
+  This function is only available if `cfg.console.enabled` is true
+
+  @class function
+]]
 function lua.reload()
   console:log("Reloading LUA engine and files")
   currentEvent:sendStateChange(States.LuaReload)
--- lua/console/state.lua
+++ lua/console/state.lua
@@ -2,20 +2,51 @@ local state = {}
 
 -- Goes back a state. If the state is the current
 -- state in the last, it will quit.
+
+--[[
+  When called, tells the engine that it should
+  go back the previous state. If the current
+  state is the last state, it will exit.
+
+  This all depends on how the State stack is built.
+
+  This function is only available if `cfg.console.enabled` is true
+
+  @class function
+]]
 function state.prev()
   console:log("Returning to previous state")
   currentEvent:sendStateChange(States.Quit)
   currentEvent:stopPropgation()
 end
 
--- Reloads the current state
+--[[
+  When called, tells the engine that it should
+  go reload the current state. This means that
+  it will unload everything in the current state before
+  creating it a new again.
+
+  This function is only available if `cfg.console.enabled` is true
+
+  @class function
+]]
 function state.reload()
   console:log("Reloading current state")
   currentEvent:sendStateChange(States.Refresh)
   currentEvent:stopPropgation()
 end
 
--- Reloads the current state and reloads the GL context
+--[[
+  When called, tells the engine that it should
+  do a full reload of the entire Window, GL context and
+  current state.
+
+  This means that the engine will basically be recreated.
+
+  This function is only available if `cfg.console.enabled` is true
+
+  @class function
+]]
 function state.windowRefresh()
   console:log("Reloading window")
   currentEvent:sendStateChange(States.WinRefresh)
