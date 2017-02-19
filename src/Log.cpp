9a80951c82334d01e7dc0f38f2f1b082daf90b96
Reewr <alexander.imenes@gmail.com>
2017-02-19 20:16:44 +0100

    Adds comments to Log

--- src/Log.cpp
+++ src/Log.cpp
@@ -9,6 +9,14 @@ std::shared_ptr<spdlog::logger> Logging::GLOBAL_LOGGER  = nullptr;
 
 Logging::Formatter::Formatter() {}
 
+/**
+ * @brief
+ *   Formats the log entry to the following format:
+ *
+ *   [timeSinceStart mm:ss.ms] [level] [name] loginfo
+ *
+ * @param msg
+ */
 void Logging::Formatter::format(spdlog::details::log_msg& msg) {
   std::string level = spdlog::level::to_str(msg.level);
   std::string name  = *msg.logger_name;
@@ -29,10 +37,24 @@ void Logging::Formatter::format(spdlog::details::log_msg& msg) {
   msg.formatted.write(spdlog::details::os::eol, spdlog::details::os::eol_size);
 }
 
+/**
+ * @brief
+ *   Creates a Logger of a specific name, using an instance of a
+ *   logger if it already exists or creating a new one.
+ *
+ * @param name
+ */
 Logging::Log::Log(const std::string& name) {
   setLoggerName(name);
 }
 
+/**
+ * @brief
+ *   Sets the name of the logger, effectively creating a new logger
+ *   if the name does not already exist.
+ *
+ * @param name
+ */
 void Logging::Log::setLoggerName(const std::string& name) {
   std::shared_ptr<spdlog::logger> logger = spdlog::get(name);
   mLog = !logger ? spdlog::stdout_color_mt(name) : logger;
@@ -40,7 +62,6 @@ void Logging::Log::setLoggerName(const std::string& name) {
 
 
 void Logging::init(spdlog::level::level_enum level, const std::string& format) {
-
   if (format != "") {
     spdlog::set_pattern(format);
   } else {
--- src/Log.hpp
+++ src/Log.hpp
@@ -6,24 +6,69 @@
 #include <string>
 
 namespace Logging {
+
+  /**
+   * @brief
+   *   This is a class that is a wrapper around an spdLogger. This is to
+   *   make it easier to integrate it into existing classes.
+   *
+   *   All you need to do enable the logger for a class is to inherit from
+   *   this class and explicitly call `Log` with the name of your choice.
+   *
+   *   Should you come into the following case:
+   *
+   *   ```
+   *   class A : public Logging::Log {
+   *   public:
+   *     A() : Logging::Log("A") {}
+   *   }
+   *
+   *   // this cannot be done as A already inherits from Log.
+   *   class B : public A, public Logging::Log {
+   *   public:
+   *     B() : Logging::Log("B") {}
+   *   }
+   *
+   *   // instead, do the following:
+   *   class B : public A {
+   *   public:
+   *     B() {
+   *       setLoggerName("B");
+   *     }
+   *   }
+   */
   class Log {
   public:
+    //! Creates an instance of Log with a specific name.
+    //! It will create a new logger if a logger with the given
+    //! name does not already exist
     Log(const std::string& name);
+
+    //! Sets the name of the logger to something different
+    //! It will create a new logger if a logger with the given
+    //! name does not already exist
     void setLoggerName(const std::string& name);
 
   protected:
     std::shared_ptr<spdlog::logger> mLog;
   };
 
+  // The formatter is used to format the output of
+  // logging
   class Formatter : public spdlog::formatter {
   public:
     Formatter();
     void format(spdlog::details::log_msg& msg);
   };
 
+  // The global logger is used in cases (through GlobalLog.hpp)
+  // where you normally don't have a logger
   extern std::shared_ptr<spdlog::logger> GLOBAL_LOGGER;
   extern unsigned int                    loggerNameSize;
 
+  // initializes the loggers, setting the minimum logging level and
+  // possibly the format. If the format is not specified, it defaults
+  // to using Formatter
   void init(spdlog::level::level_enum level  = spdlog::level::trace,
             const std::string&        format = "");
 }
