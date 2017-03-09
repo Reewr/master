f4548056d6d8fc42bfa85bc9fe563bb68890383b
Reewr <alexander.imenes@gmail.com>
2017-03-09 19:07:03 +0100

    Implements the draw function for MeshPart

--- src/3D/MeshPart.cpp
+++ src/3D/MeshPart.cpp
@@ -15,14 +15,33 @@ MeshPart::MeshPart(std::shared_ptr<Program>& program,
   if (subMesh.subMesh == nullptr)
     throw std::runtime_error("Submesh was nullptr");
 
-  mBody   = subMesh.body;
-  mShape  = mBody->getCollisionShape();
-  mMotion = mBody->getMotionState();
+  mBody    = subMesh.body;
+  mShape   = mBody->getCollisionShape();
+  mMotion  = mBody->getMotionState();
+  mProgram = program;
 }
 
 MeshPart::~MeshPart() {}
 
 void MeshPart::update(float) {}
 void MeshPart::drawShadow(Framebuffer*, Camera*) {}
-void MeshPart::draw(Camera*) {}
+
+/**
+ * @brief
+ *   Draws the mesh part. Since the MeshPart is suppose to be part of a larger
+ *   mesh, it expects a couple of things. Firstly, it expects most of the
+ *   program variables to be set. It will only set the model variable.
+ *
+ * @param c
+ */
+void MeshPart::draw(Camera* c) {
+  if (mMesh->size() == 0)
+    return;
+
+  mProgram->bind();
+  mProgram->setUniform("model", mmm::translate(mPosition) * mRotation * mScale);
+
+  mMesh->draw();
+}
+
 void MeshPart::input(const Input::Event&) {}
