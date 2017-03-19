f1aa37d555d0356f1253622bb92e564b0e6ec6da
Reewr <alexander.imenes@gmail.com>
2017-03-19 11:05:13 +0100

    Adds comments

--- src/Learning/SpiderSwarm.hpp
+++ src/Learning/SpiderSwarm.hpp
@@ -20,9 +20,25 @@ namespace NEAT {
   class Substrate;
 }
 
+/**
+ * The SpiderSwarm is a class that holds a population of spiders that are used
+ * to train a ESHyperNEAT network.
+ *
+ * @TODO Add more documentation
+ */
 class SpiderSwarm : Logging::Log {
 public:
 
+  /**
+   * A Phenotype represents a single individual in the total population. When
+   * the SpiderSwarm is initialized, a set amount of Phenotypes are created,
+   * each containing their own Spider, World and Network.
+   *
+   * The reason for having one world per spider is due to the complexity
+   * of the Spider. Even by setting up CollisionGroups and ignoring collisions
+   * between spiders, the performance is slower than having one world per
+   * spider. The downside of this approach is that it'll require more memory
+   */
   struct Phenotype {
     World*               world;
     Spider*              spider;
@@ -36,7 +52,11 @@ public:
   SpiderSwarm();
   ~SpiderSwarm();
 
+  // Updates the SpiderSwarm which will either run a normal update
+  // on the current batch or figure out which batch is next
   void update(float deltaTime);
+
+  // Draws X number of spiders from current batch set by DrawLimit
   void draw(std::shared_ptr<Program>& prog, bool bindTexture);
 
 private:
