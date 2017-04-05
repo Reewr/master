0891647ea596366c0d4f262cfe464b19dcf2b8cb
Reewr <alexander.imenes@gmail.com>
2017-04-05 20:44:06 +0200

    Adds Vertex and ColorVertex to Shape
    This structure describes a data format that is often used with the
    OpenGL shapes.

--- src/Shape/GL/Shape.hpp
+++ src/Shape/GL/Shape.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
 #include "../../OpenGLHeaders.hpp"
+#include <mmm.hpp>
 
 class GLShape {
 public:
+  struct Vertex {
+    mmm::vec3 position;
+    mmm::vec2 texCoords;
+    mmm::vec3 normals;
+  };
+
+  struct ColorVertex {
+    mmm::vec3 position;
+    mmm::vec4 color;
+    mmm::vec3 normals;
+  };
+
   GLShape();
   virtual ~GLShape();
   virtual void setup() = 0;
