a816a498f22d2185a4bfcabee1fbd2e40eb942d3
Reewr <alexander.imenes@gmail.com>
2017-04-05 20:43:11 +0200

    Adds fix for autocompletion tool
    This fix should have no effect on any builds.

--- src/OpenGLHeaders.hpp
+++ src/OpenGLHeaders.hpp
@@ -1,6 +1,16 @@
 #pragma once
 
-#include <OGL/OGL-3-3.hpp>
+// Some autocompletion tools doesnt really like OGL since it uses
+// function pointers. It is therefore better to just use the raw OpenGL headers.
+// We dont want this to happen on build though, so a define is made to make
+// this possible.
+#ifdef USE_OPENGL_RAW_HEADERS_AUTCOMPLETE
+  #define GL_GLEXT_PROTOTYPES
+  #include <GL/gl.h>
+  #include <GL/glext.h>
+#else
+  #include <OGL/OGL-3-3.hpp>
+#endif
 
 // OGL must always be first. Add line break so that clang-format doesnt
 // reorder the order of them.
