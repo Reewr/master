a969f4776e3804ae2974e4487074afc110f5525e
Reewr <alexander.imenes@gmail.com>
2017-04-07 18:24:52 +0200

    Adds 3D line

---
+++ src/3D/Line.cpp
@@ -0,0 +1,78 @@
+#include "Line.hpp"
+
+#include "../GLSL/Program.hpp"
+#include "../Graphical/Framebuffer.hpp"
+#include "../Resource/ResourceManager.hpp"
+#include "../Resource/Texture.hpp"
+#include "../Shape/GL/Line.hpp"
+#include "../Utils/Asset.hpp"
+
+using mmm::vec2;
+using mmm::vec3;
+
+Line::Line(const mmm::vec3& start, const mmm::vec3& end)
+    : Logging::Log("Line") , mLine(new GLLine()){
+  mTexture = mAsset->rManager()->get<Texture>("Texture::Line");
+
+  vec3 diffS = vec3(0,0,0) - start;
+  vec3 diffE = vec3(1,0,0) - end;
+  // find needed rotation and scale to match start and end point
+  vec3 scale = diffE - diffS;
+  if (scale.x == 0) {
+    scale.x = 1;
+  }
+  if (scale.y == 0) {
+    scale.y = 1;
+  }
+
+  if (scale.z == 0) {
+    scale.z = 1;
+  }
+
+  mLog->debug("Test: {}", scale);
+  mPosition = vec3(start + end) / 2.0;
+  mScale    = mmm::scale(mmm::abs(scale));
+}
+
+Line::~Line() {}
+
+/**
+ * @brief
+ *   The update handler is reversed for things that do not have
+ *   anything to do with drawing but should be updated every loop.
+ *
+ * @param float
+ */
+void Line::update(float) {}
+
+/**
+ * @brief
+ *   Draw the cube by binding the program and setting the required
+ *   uniforms.
+ *
+ * @param c
+ */
+void Line::draw(std::shared_ptr<Program>& program, bool bindTexture) {
+  draw(program, mmm::vec3(0), bindTexture);
+}
+
+/**
+ * @brief
+ *   Draw the cube by binding the program and setting the required
+ *   uniforms.
+ *
+ * @param c
+ */
+void Line::draw(std::shared_ptr<Program>& program,
+                mmm::vec3                 offset,
+                bool                      bindTexture) {
+  program->bind();
+  program->setUniform("model",
+                      mmm::translate(mPosition + offset) * mRotation * mScale);
+
+  if (bindTexture)
+    mTexture->bind(1);
+  mLine->draw();
+}
+
+void Line::input(const Input::Event&) {}
---
+++ src/3D/Line.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "../Drawable/Drawable3D.hpp"
+#include "../Log.hpp"
+#include <memory>
+
+#include <mmm.hpp>
+
+class GLLine;
+
+class Texture;
+class Program;
+
+/**
+ * @brief
+ *   This represents a Line. It can be specified to be
+ *   at a specified position
+ */
+class Line : public Drawable3D, public Logging::Log {
+public:
+  // Creates a cube of a specific size in meters. If weight is -1,
+  // it is assumed that the cube contains water,
+  // i.e 1m^3 (1,1,1) cube weighs 1000kg
+  //     8m^3 (2,2,2) cube weighs 8000kg
+  //
+  // The position is where the box will start at
+  Line(const mmm::vec3& start = mmm::vec3(0, 0, 0),
+       const mmm::vec3& end   = mmm::vec3(0, 0, 0));
+  ~Line();
+
+  // Update the element, physics etc
+  void update(float deltaTime);
+
+  // Draw it. Keep it separate from update
+  void draw(std::shared_ptr<Program>& program, bool bindTexture = false);
+
+  void draw(std::shared_ptr<Program>& program,
+            mmm::vec3                 offset,
+            bool                      bindTexture = false);
+
+  // Input handler for cube
+  void input(const Input::Event& event);
+
+private:
+  GLLine*                  mLine;
+  std::shared_ptr<Texture> mTexture;
+};
