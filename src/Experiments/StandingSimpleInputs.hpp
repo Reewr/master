96ae53387743ca47bc9382720160b8b333435ae3
Reewr <alexander.imenes@gmail.com>
2017-05-03 22:12:22 +0200

    Updated note about experiment

--- src/Experiments/StandingSimpleInputs.hpp
+++ src/Experiments/StandingSimpleInputs.hpp
@@ -5,7 +5,11 @@
 /**
  * @brief
  *   This experiment defines a simple experiments for creating
- *   robots that stand entirely still.
+ *   robots that stand entirely still. It only takes 8 inputs, one
+ *   for each leg of whether or not it is touching the ground or not.
+ *
+ *   No result after 2759 generations. It seems like it cant handle standing with
+ *   very inputs. This makes sense though.
  */
 class StandingSimpleInputs : public Experiment {
 public:
