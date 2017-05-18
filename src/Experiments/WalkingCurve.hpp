c02c9d00995c4afdfe4c54be059b0e79685d6150
Reewr <alexander.imenes@gmail.com>
2017-05-18 16:46:04 +0200

    Removed incorrect comments

--- src/Experiments/WalkingCurve.hpp
+++ src/Experiments/WalkingCurve.hpp
@@ -2,16 +2,6 @@
 
 #include "Experiment.hpp"
 
-/**
- * @brief
- *   This experiment defines an experiment using HyperNEAT
- *   to make the robot walk using a 2D substrate that takes
- *   an input for each active joint and gets its rotations as a
- *   normalized value between -1 and 1 where -1 indicates -1*PI.
- *
- *   In addition, it also takes an input of whether a leg is toching
- *   the ground or not.
- */
 class WalkingCurve : public Experiment {
 public:
   WalkingCurve();
